0eeb0c78024adff89fc185ed30fafdea95892cd8
Matt Franchi <mattwfranc@gmail.com>
2023-09-22 14:29:54 -0400

    added comments to pull nyc graph script

--- src/utils/pull_nyc_graph.py
+++ src/utils/pull_nyc_graph.py
@@ -1,8 +1,19 @@
+# FARLAB -- UrbanECG Project
+# Dev: Matt Franchi, help from GitHub Copilot 
+# Last Edited: 09/14/2023 
+
+# This script is used to pull the graph of New York City from OpenStreetMap -- should update with time. 
+
+# Imports 
 import osmnx as ox 
 import logging 
 import os
 
+
 def pull_nyc_graph(): 
+    """
+    Pull the graph of New York City from OpenStreetMap. 
+    """
     # Get the graph of New York City
     logging.info("Pulling graph of NYC from OSM.")
     G = ox.graph_from_place("New York City, New York, USA", network_type="drive")
