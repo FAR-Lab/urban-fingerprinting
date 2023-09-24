e0ae584fb2e402e68a21fa694e8de245376887c4
Matt Franchi <mattwfranc@gmail.com>
2023-09-24 18:47:21 -0400

    graph2prs script works for first set of anl data

--- src/analysis/graph_to_prs.py
+++ src/analysis/graph_to_prs.py
@@ -9,6 +9,7 @@ import numpy as np
 import geopandas as gpd 
 import osmnx as ox 
 import os
+import sys 
 from glob import glob 
 
 from fire import Fire
@@ -22,6 +23,12 @@ import datetime
 # Set up logging
 import logging
 logging.basicConfig(level=logging.INFO)
+
+from shapely import wkt
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from utils import coco_mappings as cm
 
 
 # Constant for number of workers to use in parallel, should be equal to number of cores on machine
@@ -60,6 +67,7 @@ class DayOfCoverage:
         self.frames_data = [] 
         self.nearest_edges = [] 
         self.nearest_edges_dist = []
+        self.detections = []
 
 
 class G:
@@ -104,7 +112,7 @@ class G:
     def __init__(self, proj_path, graphml_input):
 
         self.DEBUG_MODE = False 
-        self.WRITE_MODE = True 
+        self.WRITE_MODE = False 
         self.log = logging.getLogger(__name__)
         self.log.info(f'Loading graph at path {graphml_input}')
         self.PROJ_PATH = proj_path 
@@ -147,17 +155,16 @@ class G:
     
             # Read CSV
             df = pd.read_csv(md_csv, engine='pyarrow')
-            gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df["gps_info.longitude"], df["gps_info.latitude"], crs="EPSG:4326"))
-            gdf = gdf.to_crs("EPSG:2263")
     
-            return gdf
+            return df
 
-    def get_data(self, day_of_coverage, num_workers=8):
+    def get_data(self, day_of_coverage, num_workers=24):
 
         # Check if md file has already been written to output 
         if os.path.exists(f"../../output/df/{day_of_coverage}/md.csv"):
             # read md file from output 
             md = pd.read_csv(f"../../output/df/{day_of_coverage}/md.csv", engine='pyarrow')
+            md = gpd.GeoDataFrame(md, geometry=wkt.loads(md['geometry']), crs="EPSG:4326")
             # return md
             self.log.info(f"Loading metadata from output for day of coverage {day_of_coverage}.")
             return md
@@ -210,6 +217,10 @@ class G:
         frame_ids = [x.split("/")[-1].split(".")[0] for x in frames]
         # filter md list to only include rows in frame_ids 
         md = md[md["frame_id"].isin(frame_ids)]
+
+        # turn md into GeoDataFrame
+        md = gpd.GeoDataFrame(md, geometry=gpd.points_from_xy(md["gps_info.longitude"], md["gps_info.latitude"], crs="EPSG:4326"))
+        md = md.to_crs("EPSG:2263")
        
         if self.WRITE_MODE:
             os.makedirs(f"../../output/df/{day_of_coverage}", exist_ok=True)
@@ -259,6 +270,7 @@ class G:
 
         # Get day of coverage data
         md = self.get_day_of_coverage(day_of_coverage).frames_data
+        md = md.to_crs("EPSG:2263")
 
         # Check if nearest edges have already been written to output
         if os.path.exists(f"../../output/df/{day_of_coverage}/nearest_edges.csv"):
@@ -271,17 +283,18 @@ class G:
 
         md_split = np.array_split(md, 10 * NUM_CORES)
 
+
         nearest = Parallel(n_jobs=NUM_CORES)(delayed(self.nearest_road_worker)(subset) for subset in tqdm(md_split))
         nearest_edges, nearest_edges_dist = zip(*nearest)
 
         nearest_edges = [item for sublist in nearest_edges for item in sublist]
         nearest_edges_dist = [item for sublist in nearest_edges_dist for item in sublist]
-        print(nearest_edges[:5])
-        print(nearest_edges_dist[:5])
+
 
         nearest_edges = pd.DataFrame(nearest_edges, columns=['u', 'v', 'key'])
         nearest_edges['dist'] = nearest_edges_dist
-        print(nearest_edges.head())
+        nearest_edges['frame_id'] = md['frame_id'].tolist()
+        nearest_edges = nearest_edges.set_index('frame_id')
 
         DoC.nearest_edges = nearest_edges
 
@@ -294,6 +307,14 @@ class G:
         self.log.info(f"Added nearest edges to day of coverage {day_of_coverage}.")
         return 0
     
+    def add_detections(self, day_of_coverage):
+        detections = pd.read_csv(f"../../output/df/{day_of_coverage}/detections.csv", engine='pyarrow')
+        DoC = self.get_day_of_coverage(day_of_coverage)
+        DoC.detections = detections
+        self.log.info(f"Added detections to day of coverage {day_of_coverage}.")
+        return 0
+    
+    
 
     def plot_edges(self): 
         _, ax = plt.subplots(figsize=(20,20))
@@ -304,7 +325,7 @@ class G:
     
     def plot_coverage(self, day_of_coverage):
         DoC = self.get_day_of_coverage(day_of_coverage)
-        _, ax = plt.subplots(figsize=(30,30), frameon=False)
+        _, ax = plt.subplots(figsize=(30,30))
 
         # group by nearest edge, plot chloropleth 
         print(DoC.nearest_edges.columns)
@@ -330,10 +351,52 @@ class G:
         rID = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
         plt.savefig(f"../../output/plots/coverage_{rID}.png", bbox_inches='tight', pad_inches=0)
         plt.close()
+    
+    def plot_detections(self, day_of_coverage, class_id):
+        DoC = self.get_day_of_coverage(day_of_coverage)
+        _, ax = plt.subplots(figsize=(30,30), frameon=True)
+
+        coverage = DoC.nearest_edges.merge(DoC.detections, left_index=True, right_index=True)
+
+        coverage = coverage.groupby(['u', 'v']).agg({str(class_id): 'mean'}).reset_index()
+        
+
+        #coverage['binned'] = pd.qcut(coverage[str(class_id)], 100, labels=False, duplicates='drop')
+
+        self.gdf_edges.plot(ax=ax, color='lightcoral', linewidth=0.5, alpha=0.2)
+        self.gdf_edges.merge(coverage, on=['u', 'v'], how='left').plot(ax=ax, column=str(class_id), cmap='cividis', linewidth=0.5, legend=True,
+                                                                                 legend_kwds={'label': "Number of frames", 'orientation': "horizontal"})
+        
+        ax.set_axis_off()
+        ax.margins(0)
+        ax.set_title(f"Average # of {cm.coco_classes[class_id]}s for {day_of_coverage}")
+        ax.title.set_size(50)
+
+
+        rID = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+        os.makedirs(f"../../output/plots/{day_of_coverage}", exist_ok=True)
+        plt.savefig(f"../../output/plots/{day_of_coverage}/{class_id}_density_{rID}.png", bbox_inches='tight', pad_inches=0)
+        plt.close()
+
+    def init_day_of_coverage(self, doc):
+        self.add_day_of_coverage(doc)
+        self.coverage_to_nearest_road(doc)
+        self.add_detections(doc)
+
+        self.log.info(f"Added {doc} to graph.")
+
+
+
+
 
 
 if __name__ == '__main__':
+    days_of_coverage = ["2023-08-10", "2023-08-11", "2023-08-12", "2023-08-13", "2023-08-14", "2023-08-17", "2023-08-18", "2023-08-20", "2023-08-21", "2023-08-22", "2023-08-23", "2023-08-24", "2023-08-28", "2023-08-29", "2023-08-30", "2023-08-31"]
     graph = G("/share/ju/nexar_data/nexar-scraper","/share/ju/urbanECG/data/geo/nyc.graphml")
-    graph.add_day_of_coverage("2023-08-12")
-    graph.coverage_to_nearest_road("2023-08-12")
-    graph.plot_coverage("2023-08-12")
+    for day in days_of_coverage:
+        try:
+            graph.init_day_of_coverage(day)
+        except Exception as e:
+            graph.log.error(f"Error in {day}: {e}")
+   
+
