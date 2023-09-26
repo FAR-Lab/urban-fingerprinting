705b80efb06e13bf013adf1490536b36dd3500f5
Matt Franchi <mattwfranc@gmail.com>
2023-09-26 16:53:01 -0400

    bad practice whoops, 9/26 SoC

--- src/analysis/Graph.py
+++ src/analysis/Graph.py
@@ -11,6 +11,9 @@ import osmnx as ox
 import os
 import sys 
 from glob import glob 
+from PIL import Image
+
+import contextlib
 
 from fire import Fire
 from tqdm import tqdm
@@ -19,6 +22,7 @@ import uuid
 from joblib import Parallel, delayed
 from itertools import chain 
 import datetime 
+import pytz
 
 # Set up logging
 import logging
@@ -40,7 +44,7 @@ if NUM_CORES is None:
     # Set to 8
     NUM_CORES = 8
 else: 
-    NUM_CORES *= 2
+    NUM_CORES = int(NUM_CORES)
 
 
 
@@ -100,6 +104,11 @@ class G:
     def toggle_debug(self):
         self.DEBUG_MODE = not self.DEBUG_MODE
         self.log.info(f"Debug mode set to {self.DEBUG_MODE}.")
+
+    def toggle_latex_font(self):
+        plt.rcParams['text.usetex'] = not plt.rcParams['text.usetex']
+        self.log.info(f"Latex font set to {plt.rcParams['text.usetex']}.")
+
     
     def get_frames_worker(self, folder):
 
@@ -302,8 +311,7 @@ class G:
         _, ax = plt.subplots(figsize=(30,30))
 
         # group by nearest edge, plot chloropleth 
-        print(DoC.nearest_edges.columns)
-        print(DoC.nearest_edges.head())
+        
 
         coverage = DoC.nearest_edges.groupby(['u', 'v']).size().reset_index(name='counts')
 
@@ -343,7 +351,7 @@ class G:
         
         ax.set_axis_off()
         ax.margins(0)
-        ax.set_title(f"Average # of {cm.coco_classes[class_id]}s for {day_of_coverage}")
+        ax.set_title(f"Average Num. of {cm.coco_classes[class_id]}s for {day_of_coverage}")
         ax.title.set_size(50)
 
 
@@ -365,6 +373,270 @@ class G:
         gif.generate_frames("captured_at", "2", "10min", "bin", car_offset=True)
         gif.generate_gif()
 
+    def join_days(self, DoCs): 
+        # Check if all days are in graph
+        for doc in DoCs:
+            if self.get_day_of_coverage(doc) == 4:
+                self.log.error(f"Day of coverage {doc} not in graph.")
+                return 4
+        
+        # Get nearest edges for each day of coverage
+        nearest_edges = []
+        for doc in DoCs:
+            nearest_edges.append(self.get_day_of_coverage(doc).nearest_edges)
+        
+        # Concatenate nearest edges 
+        nearest_edges = pd.concat(nearest_edges)
+
+        # Get detections for each day of coverage
+        detections = []
+        for doc in DoCs:
+            detections.append(self.get_day_of_coverage(doc).detections)
+        
+        # Concatenate detections
+        detections = pd.concat(detections)
+
+        # Get metadata for each day of coverage
+        md = []
+        for doc in DoCs:
+            md.append(self.get_day_of_coverage(doc).frames_data)
+        
+        # Concatenate metadata
+        md = pd.concat(md)
+        md['captured_at'] = pd.to_datetime(md['captured_at'], unit='ms')
+        md['captured_at'] = md['captured_at'].dt.tz_localize('UTC').dt.tz_convert('America/New_York')
+
+        return DayOfCoverage("joined", md, nearest_edges, detections)
+
+
+    def coco_agg_mappings(self, operation='mean', data = pd.DataFrame()):
+        agg_dict = {}
+        for class_id in cm.coco_classes.keys():
+            if str(class_id) in data.detections.columns.astype(str):
+                agg_dict[str(class_id)] = operation
+        return agg_dict
+
+
+    def density_per_road_segment(self, DoCs, dtbounds=(datetime.datetime(1970,1,1,0,0,0), datetime.datetime(2024,1,1,0,0,0))): 
+        # Make sure dtbounds are localized in EST 
+        dtbounds = (dtbounds[0].astimezone(tz=pytz.timezone('America/New_York')), dtbounds[1].astimezone(tz=pytz.timezone('America/New_York')))
+        data = self.join_days(DoCs)
+
+        md = data.frames_data
+        
+
+        md = md[(md['captured_at'] >= dtbounds[0]) & (md['captured_at'] <= dtbounds[1])]
+
+        
+        detections = data.detections 
+        detections.set_index(detections.iloc[:,0], inplace=True)
+        detections.fillna(0, inplace=True)
+
+       
+
+        density = md.merge(detections, left_on='frame_id', right_index=True)
+        density = density.merge(data.nearest_edges, left_on='frame_id', right_on='frame_id')
+        
+
+
+
+        density = density.groupby(['u', 'v']).agg(self.coco_agg_mappings(data=data)).reset_index()
+
+        os.makedirs(f"../../output/df/density", exist_ok=True)
+        density.describe().to_csv("../../output/df/density/density_describe.csv")
+        density.to_csv("../../output/df/density/density.csv")
+
+        del data
+        del detections
+        del md 
+
+        return density 
+
+    def data2density(self, data, class_id, dtbounds, car_offset=False):
+
+
+        if car_offset:
+            data.detections[str(class_id)] = data.detections[str(class_id)] + 1
+
+        md = data.frames_data
+        
+
+        md = md[(md['captured_at'] >= dtbounds[0]) & (md['captured_at'] <= dtbounds[1])]
+
+        
+        detections = data.detections 
+        detections.set_index(detections.iloc[:,0], inplace=True)
+        detections.fillna(0, inplace=True)
+
+
+        density = md.merge(detections, left_on='frame_id', right_index=True)
+        density = density.merge(data.nearest_edges, left_on='frame_id', right_on='frame_id')
+        
+
+
+
+        density = density.groupby(['u', 'v']).agg(self.coco_agg_mappings(data=data)).reset_index()
+
+        del md 
+        del detections 
+        del data 
+
+        return density
+        
+
+
+
+    def plot_density_per_road_segment(self, output_dir, DoCs, bin, density, class_id, binned=True, car_offset=False): 
+
+        density = self.gdf_edges.merge(density, on=['u', 'v'], how='left')
+        density = density.to_crs("EPSG:2263")
+
+
+
+        _, ax = plt.subplots(figsize=(40,40), frameon=True)
+
+        self.gdf_edges.plot(ax=ax, color='lightcoral', linewidth=0.5, alpha=0.2)
+
+        density['binned'] = pd.cut(density.loc[:, str(class_id)], 100, duplicates='drop')
+        # convert bins into upper bound
+        density['binned'] = density['binned'].apply(lambda x: x.right)
+
+
+
+
+        density.plot(ax=ax, column='binned' if binned else str(class_id), cmap='BuPu', linewidth=1.5, legend=False)
+
+        # create custom legend based on bins 
+        bins = list(density['binned'].unique())
+        bins.sort()
+        
+        # create custom, continuous legend 
+        # create a color map
+        cmap = plt.cm.BuPu
+        # extract all colors from the .jet map
+        cmaplist = [cmap(i) for i in range(cmap.N)]
+        # create the new map
+        cmap = cmap.from_list('Custom cmap', cmaplist, cmap.N)
+       
+        # don't use bins, use a static min and max 
+        bounds = np.linspace(0, 1, 60)
+        norm = plt.Normalize(bounds.min(), bounds.max())   
+        
+        # create a second axes for the colorbar, on left side 
+        ax2 = ax.figure.add_axes([0.05, 0.05, 0.03, 0.9])
+        
+        cb = plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=cmap), cax=ax2)
+        cb.ax.tick_params(labelsize=50)
+        cb.ax.yaxis.set_ticks_position('right')
+        cb.ax.yaxis.set_label_position('right')
+        cb.ax.invert_yaxis()
+        cb.ax.set_yticklabels([f"{float(x):.2f}" for x in cb.get_ticks()])
+        
+        
+        cb.ax.title.set_fontsize(50)
+        cb.ax.title.set_horizontalalignment('center')
+        cb.ax.title.set_verticalalignment('bottom')
+
+        ax.set_axis_off()
+        ax.margins(0)
+        ax.set_title(f"Average Num. of {cm.coco_classes[str(class_id)]}s per road segment \n {DoCs[0]}-{DoCs[-1]} \n {bin}")
+        ax.title.set_size(50)
+        # Move title up 
+        ax.title.set_position([.5, 1.05])
+
+        rID = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+        os.makedirs(f"../../output/plots/{output_dir}", exist_ok=True)
+        plt.savefig(f"../../output/plots/{output_dir}/{class_id}_density_{rID}.png", bbox_inches='tight', pad_inches=0)
+        plt.clf()
+        plt.close()
+
+    
+
+    def density_over_time_gif(self, docs, dtbounds, class_id, delta="60min", car_offset=True):
+        class_id = str(class_id)
+        data = self.join_days(docs)
+
+        # generate time bins 
+        bins = pd.date_range(dtbounds[0], dtbounds[1], freq=delta)
+        bins = bins.tz_localize('America/New_York')
+
+        output_dir = uuid.uuid4().hex[:8]
+
+        args = [] 
+        for idx, bin in enumerate(bins): 
+            # Sliding window 
+            if idx < 3: 
+                dtbounds = (bins[0], bins[idx])
+            else:
+                dtbounds = (bins[idx-3], bins[idx])
+            dtbounds = (bin, bin + pd.Timedelta(delta))
+            plot_data = data.frames_data[(data.frames_data['captured_at'] >= dtbounds[0]) & (data.frames_data['captured_at'] <= dtbounds[1])]
+            density = self.data2density(data, class_id, dtbounds, car_offset=car_offset)
+            del plot_data 
+            args.append((output_dir, docs, bin, density, class_id, car_offset))
+        
+
+
+        Parallel(n_jobs=NUM_CORES)(delayed(self.plot_density_per_road_segment_parallel)(arg) for arg in tqdm(args))
+
+        # generate gif
+        self.generate_gif(output_dir, class_id, docs, delta)
+        
+    def plot_density_per_road_segment_parallel(self, args): 
+        output_dir, docs, bin, density, class_id, car_offset = args
+        try:
+            graph.plot_density_per_road_segment(output_dir, docs, bin, density, class_id, car_offset=car_offset)
+            del args
+            del density
+        except Exception as e:
+            graph.log.error(f"Error in {bin}: {e}")
+            return 4
+
+    def generate_gif(self, frames_dir, class_id, days_of_coverage, delta, fps=24, duration=42, loop=0):
+        
+        os.makedirs(f"../../output/gifs", exist_ok=True)
+
+        now = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+
+        if not frames_dir: 
+            self.log.error("No frames directory found.")
+            return
+
+        fp_out = f"../../output/gifs/{class_id}_{frames_dir}_{now}.gif"
+
+        with contextlib.ExitStack() as stack:
+
+            # lazily load images
+            imgs = (stack.enter_context(Image.open(f))
+                    for f in sorted(glob(f"../../output/plots/{frames_dir}/*.png")))
+
+            # extract  first image from iterator
+            img = next(imgs)
+
+            img.save(fp=fp_out, format='GIF', append_images=imgs,
+                    save_all=True, duration=42, loop=0)
+
+        
+
+        
+        
+        
+
+            
+
+    
+
+                                            
+
+
+
+
+
+
+    
+
+
+            
 
 
 
@@ -376,11 +648,16 @@ if __name__ == '__main__':
     #days_of_coverage = ["2023-08-10", "2023-08-11", "2023-08-12", "2023-08-13", "2023-08-14", "2023-08-17", "2023-08-18", "2023-08-20", "2023-08-21", "2023-08-22", "2023-08-23", "2023-08-24", "2023-08-28", "2023-08-29", "2023-08-30", "2023-08-31"]
     days_of_coverage = ["2023-08-10", "2023-08-11", "2023-08-12"]
     graph = G("/share/ju/nexar_data/nexar-scraper","/share/ju/urbanECG/data/geo/nyc.graphml")
+    graph.toggle_latex_font()
     for day in days_of_coverage:
         try:
             graph.init_day_of_coverage(day)
         except Exception as e:
             graph.log.error(f"Error in {day}: {e}")
-    graph.generate_gif(days_of_coverage)
+    #density = graph.density_per_road_segment(days_of_coverage, dtbounds=(datetime.datetime(2023,8,10,12,0,0), datetime.datetime(2023,8,10,14,0,0)))
+    #graph.plot_density_per_road_segment(days_of_coverage, density, 2, car_offset=True)
+
+    graph.density_over_time_gif(days_of_coverage, (datetime.datetime(2023,8,10,1,0,0), datetime.datetime(2023,8,11,0,0,0)), 2, delta="10min", car_offset=True)    
+
    
 
