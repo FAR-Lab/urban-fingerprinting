b7b0e2541fbbff67b5a2408dbf0d1ab28c23f65c
Matt Franchi <mattwfranc@gmail.com>
2023-10-09 16:25:28 -0400

    within month plot

--- src/visualization/CoverageCard.py
+++ src/visualization/CoverageCard.py
@@ -16,6 +16,10 @@ import matplotlib.colors as mcolors
 from matplotlib import cm
 import matplotlib as mpl
 
+from pathlib import Path
+
+import calendar 
+
 import sys 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
@@ -28,7 +32,7 @@ import datetime
 
 # CoverageCard class
 class CoverageCard(): 
-    def __init__(self, days_of_coverage): 
+    def __init__(self, days_of_coverage, attached_plot=""): 
         # CASE: list of Y-m-d strings
         if all(map(lambda x: isinstance(x, str), days_of_coverage)):
             # convert strings to datetimes
@@ -49,6 +53,8 @@ class CoverageCard():
 
         # calculate time delta between earliest and latest days of coverage
         self.delta = self.days_of_coverage[-1] - self.days_of_coverage[0]
+
+        self.attached_plot = attached_plot
     
     def latex(self, latex=True):
         """
@@ -131,7 +137,73 @@ class CoverageCard():
 
 
     def plot_within_month(self):
-        pass
+
+        earliest_day = self.days_of_coverage[0]
+
+        fig, ax = plt.subplots(figsize=(9, 9))
+
+        box_height = 0.5 
+        box_width = 0.5 
+
+        outer_padding_h = 0.05
+        outer_padding_v = 0.05
+
+        # set axis limits
+        ax.set_xlim(0, 7*box_height + 2*outer_padding_h)
+        ax.set_ylim(0, 5*box_width + 2*outer_padding_v)
+
+        ax.set_frame_on(False)
+
+        # no axis ticks 
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+        # get number of days in month 
+        num_days_in_month = calendar.monthrange(earliest_day.year, earliest_day.month)[1]
+        for day in [earliest_day + datetime.timedelta(days=x) for x in range(num_days_in_month)]:
+            row = 4 - int((day.day + earliest_day.weekday()) / 7)
+            col = (day.weekday() + 1) % 7
+
+            if day in self.days_of_coverage:
+                ax.add_patch(mpatches.Rectangle((col*box_width + outer_padding_h, row*box_height + outer_padding_v), box_width, box_height, facecolor='green', alpha=0.5, capstyle='round'))
+                # add shadow around patch 
+                ax.add_patch(mpatches.Rectangle((col*box_width + outer_padding_h, row*box_height + outer_padding_v), box_width, box_height, facecolor='none', edgecolor='grey', linewidth=2, alpha=0.5, capstyle='round')) 
+                
+                # add label inplace 
+                ax.text(col*box_width + outer_padding_h + box_width/2, row*box_height + outer_padding_v + box_height/2, day.day, horizontalalignment='center', verticalalignment='center')
+            else:
+                ax.add_patch(mpatches.Rectangle((col*box_width + outer_padding_h, row*box_height + outer_padding_v), box_width, box_height, facecolor='red', alpha=0.15))
+                ax.add_patch(mpatches.Rectangle((col*box_width + outer_padding_h, row*box_height + outer_padding_v), box_width, box_height, facecolor='none', edgecolor='grey', linewidth=1.5, alpha=0.5, capstyle='round')) 
+                # add label inplace
+                ax.text(col*box_width + outer_padding_h + box_width/2, row*box_height + outer_padding_v + box_height/2, day.day, horizontalalignment='center', verticalalignment='center')
+                
+
+        # plot legend
+        green_patch = mpatches.Patch(color='green', label='Day is in Plot')
+        red_patch = mpatches.Patch(color='red', label='Day Excluded')
+        plt.legend(handles=[green_patch, red_patch], loc='lower right', fancybox=True, framealpha=1)
+        # move legend down
+      
+
+        # add title with month and year 
+        if self.attached_plot != "": 
+            ax.set_title(self.attached_plot + "\n" + earliest_day.strftime("%B %Y")+'\n')
+        else:
+            ax.set_title(earliest_day.strftime("%B %Y")+"\n")
+        
+        # add days of week labels below title, and above the first row of data 
+        for i in range(7):
+            i_day = (i-1) % 7
+            ax.text(i*box_width + outer_padding_h + box_width/2, 4.6*box_height + outer_padding_v + box_height/2, calendar.day_abbr[i_day], horizontalalignment='center', verticalalignment='center')
+
+    
+
+
+        plt.savefig(f'{Path(self.attached_plot).stem}_coverage_card.png')
+
+
+
+
 
     def plot_within_year(self):
         pass
