e23d51d97c32378d402e6241e23c2a49eaaee9bc
Matt Franchi <mattwfranc@gmail.com>
2023-10-21 17:27:02 -0400

    src/tests update (need to refactor into /tests

--- src/test/graph-test.py
+++ src/test/graph-test.py
@@ -15,7 +15,7 @@ import datetime
 # all days of coverage pulled in august with coverage > 0.95 
 augDoCs = ["2023-08-10", "2023-08-11", "2023-08-12", "2023-08-13", "2023-08-14", "2023-08-17", "2023-08-18", "2023-08-20", "2023-08-21", "2023-08-22", "2023-08-23", "2023-08-24", "2023-08-28", "2023-08-29", "2023-08-30", "2023-08-31"]
 # tunable subset of days of coverage for quicker testing 
-augDoCs_subset = augDoCs[0:3]
+augDoCs_subset = augDoCs[0:1]
 
 # parent directory with 'YYYY-MM-DD' subdirectories of frames 
 FRAMES_DIR = "/share/ju/nexar_data/nexar-scraper"
@@ -39,4 +39,4 @@ if __name__ == "__main__":
         except Exception as e:
             graph.log.error(f"Error in {day}: {e}")
 
-    graph.density_over_time_of_day_gif(DoCs, (datetime.datetime(2023,8,10,0,0,0), datetime.datetime(2023,8,10,23,59,59)), 2, delta="60min", car_offset=True)
+    graph.density_over_time_of_day_gif(DoCs, (datetime.datetime(2023,8,10,0,0,0), datetime.datetime(2023,8,10,23,59,59)), 2, delta="5min", car_offset=True)
