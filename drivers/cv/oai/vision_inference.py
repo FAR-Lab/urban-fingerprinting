d46a8707e0254aa6c41b2de7a533dd8d10f03fe9
Matt Franchi <mattwfranc@gmail.com>
2023-11-28 13:22:04 -0500

    support for multiple images

--- drivers/cv/oai/vision_inference.py
+++ drivers/cv/oai/vision_inference.py
@@ -16,6 +16,8 @@ sys.path.append(
     os.path.abspath(os.path.join(os.getcwd(), os.pardir, os.pardir, os.pardir))
 )
 
+from glob import glob
+from random import shuffle 
 from src.cv.oai.vision_inference import OAI_Session
 
 import asyncio
@@ -23,9 +25,20 @@ import asyncio
 if __name__ == '__main__': 
 
     img_path = "/share/ju/nexar_data/training_datasets/flooding_CLIP_336/87ca38a717a92881d0919956b54fb697_flooded road_0.7010676860809326.jpg"
+    #img_dir = "/share/ju/nexar_data/training_datasets/flooding_CLIP_336/"
+    #imgs = glob(img_dir + "*.jpg")
+
+    NUM_IMGS_TO_INFER = 1
+
+    #shuffle(imgs)
+    #imgs = imgs[:NUM_IMGS_TO_INFER]
 
     async def main(): 
         async with OAI_Session() as session: 
-            print(await session.infer_image(img_path))
+
+            #tasks = [session.infer_image(img) for img in imgs]
+            tasks = [await session.infer_image(img_path)]
+
+            return tasks 
 
     asyncio.run(main())
