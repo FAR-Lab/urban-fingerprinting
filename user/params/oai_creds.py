eed1506601c3639477b9290276690d39409cf102
Matt Franchi <mattwfranc@gmail.com>
2023-11-28 11:23:41 -0700

    Update oai_creds.py

--- user/params/oai_creds.py
+++ user/params/oai_creds.py
@@ -1,3 +1,5 @@
 # NOTE: THIS SHOULD NOT BE CHECKED INTO GIT, CONTAINS API KEY
 
+# This key was mistakenly uploaded, will not work, has been disabled by OpenAI. .gitignore had wrong path, should be fixed now. 
+
 OAI_KEY = "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"
